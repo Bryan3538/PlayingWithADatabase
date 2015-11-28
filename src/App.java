dbdf4b1deed7ac39576bd1fa0be4b79f65c7b7ff
Bryan Fulton <bryan3538@comcast.net>
2015-11-28 16:06:07 -0500

    Implement basic CRUD operations for the EmployeeRepository

--- src/App.java
+++ src/App.java
@@ -7,6 +7,7 @@ import Models.Location;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Bryan on 11/21/2015.
@@ -39,6 +40,58 @@ public class App {
         System.out.println("--------------------------------");
         emp = empRepo.findEmployeeByID(-1);
         System.out.println(emp);
+        System.out.println("--------------------------------");
+        //INSERT STATEMENT
+        System.out.println("INSERT");
+        Employee newEmp = new Employee();
+        newEmp.setID(100);
+        newEmp.setFirstName("I'm New!");
+        newEmp.setLastName("Newbie");
+        newEmp.setDept(1);
+        newEmp.setLocation(1);
 
+        try {
+            if(empRepo.saveEmployee(newEmp)) {
+                System.out.println("Employee successfully inserted!");
+                Employee someEmp = empRepo.findEmployeeByID(100);
+                System.out.println(someEmp);
+            } else
+                System.out.println("Your employee could not be inserted!");
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        }
+
+
+        System.out.println("--------------------------------");
+        System.out.println("UPDATE");
+        newEmp.setFirstName(generateName());
+        newEmp.setLastName(generateName());
+        int affected = empRepo.updateEmployee(newEmp);
+
+        if(affected >= 0) {
+            System.out.println("Something was updated!");
+            Employee someEmp = empRepo.findEmployeeByID(newEmp.getID());
+            System.out.println(someEmp);
+        } else {
+            System.out.println("Nothing was updated!");
+        }
+
+
+        System.out.println("--------------------------------");
+        System.out.println("DELETE");
+
+        if(empRepo.deleteEmployee(newEmp.getID()) >= 0) {
+            System.out.println("Employee was successfully deleted!");
+            newEmp = empRepo.findEmployeeByID(newEmp.getID());
+            System.out.println(newEmp);
+        } else
+            System.out.println("Employee was not deleted");
+
+
+    }
+
+    private static String generateName() {
+        String[] names = new String[] {"John", "Jim", "Jack", "Jane", "Jessica", "Jasmine"};
+        return names[new Random().nextInt(names.length)];
     }
 }
--- src/Data/EmployeeRepository.java
+++ src/Data/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package Data;
 
-import Models.Department;
 import Models.Employee;
 import Models.Location;
 
@@ -13,9 +12,34 @@ import java.util.List;
  * Created by Bryan on 11/21/2015.
  */
 public class EmployeeRepository {
-    //I know this should be separate for different tables and such, but I'm being lazy for testing purposes :)
 
-    //Employee stuff
+    //Create
+    public boolean saveEmployee(Employee emp) throws SQLException {
+        DataSource ds = DataSourceFactory.getMySqlDataSource();
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO employees(ID, FirstName, LastName, DepartmentNumber, LocationID) VALUES (?, ?, ?, ?, ?)";
+        int affected = 0;
+
+        try {
+            conn = ds.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, emp.getID());
+            stmt.setString(2, emp.getFirstName());
+            stmt.setString(3, emp.getLastName());
+            stmt.setInt(4, emp.getDept());
+            stmt.setInt(5, emp.getLocation());
+
+            affected = stmt.executeUpdate();
+        } finally {
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        }
+
+        return (affected >= 1);
+    }
+
+    //Read
     public List<Employee> getAllEmployees() {
         List<Employee> emps = new ArrayList<>();
         Employee emp;
@@ -88,5 +112,55 @@ public class EmployeeRepository {
         return emp;
     }
 
+    //Update
+    public int updateEmployee(Employee emp) throws SQLException {
+        Employee existing = findEmployeeByID(emp.getID());
+        DataSource ds = null;
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        int affected = 0;
 
+        if (existing != null) {
+            try {
+                ds = DataSourceFactory.getMySqlDataSource();
+                conn = ds.getConnection();
+
+                stmt = conn.prepareStatement("UPDATE employees SET FirstName=?, LastName=?, DepartmentNumber=?, LocationID=? WHERE ID=?");
+                stmt.setString(1, emp.getFirstName());
+                stmt.setString(2, emp.getLastName());
+                stmt.setInt(3, emp.getDept());
+                stmt.setInt(4, emp.getLocation());
+                stmt.setInt(5, emp.getID());
+
+                affected = stmt.executeUpdate();
+            } finally {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            }
+        }
+
+        return affected;
+    }
+
+    //Delete
+    public int deleteEmployee(int id) throws SQLException {
+        DataSource ds = null;
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        String sql = "DELETE FROM employees WHERE ID=?";
+        int affected = 0;
+
+        try {
+            ds = DataSourceFactory.getMySqlDataSource();
+            conn = ds.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            affected = stmt.executeUpdate();
+        } finally {
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        }
+
+        return affected;
+    }
 }
